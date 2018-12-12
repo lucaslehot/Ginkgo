ff44ee015239075bb4f4c7264f52f1e3101e0dc1
lucaslehot <lehotlucas@gmail.com>
2018-12-12 18:28:16 +0100

    1st feature : lighting

--- content_scripts/app.js
+++ content_scripts/app.js
@@ -1,8 +1,21 @@
-console.log('hey')
+const GKbodyContent = document.querySelectorAll("body > *")
+const GKbody = document.querySelector("body")
 
-const buttons = document.querySelectorAll("button")
+const GKcontainer = document.createElement("div")
+const GKlightingLeft = document.createElement("div")
+const GKlightingRight = document.createElement("div")
+GKlightingLeft.classList.add("GKlighting", "left")
+GKlightingRight.classList.add("GKlighting", "right")
+GKbody.appendChild(GKlightingLeft)
+GKbody.appendChild(GKlightingRight)
 
-for (let i=0;i<buttons.lenght;i++)
+
+
+GKbody.appendChild(GKcontainer)
+GKbody.classList.add("GKbody")
+GKcontainer.classList.add("GKcontainer")
+
+for (let i = 0 ; i < GKbodyContent.length ; i++)
 {
-  buttons[i].classList.add('clickable')
+  GKcontainer.appendChild(GKbodyContent[i])
 }
