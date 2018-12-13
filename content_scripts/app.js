f93faff67c8254b563a06501649fe506935a1439
lucaslehot <lehotlucas@gmail.com>
2018-12-13 15:54:38 +0100

    removed bad feature

--- content_scripts/app.js
+++ content_scripts/app.js
@@ -6,27 +6,27 @@
 ** LUCAS' SANDBOX ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
-const GKbodyContent = document.querySelectorAll("body > *")
-const GKbody = document.querySelector("body")
-
-const GKcontainer = document.createElement("div")
-const GKlightingLeft = document.createElement("div")
-const GKlightingRight = document.createElement("div")
-GKlightingLeft.classList.add("GKlighting", "left")
-GKlightingRight.classList.add("GKlighting", "right")
-GKbody.appendChild(GKlightingLeft)
-GKbody.appendChild(GKlightingRight)
-
-
-
-GKbody.appendChild(GKcontainer)
-GKbody.classList.add("GKbody")
-GKcontainer.classList.add("GKcontainer")
-
-for (let i = 0 ; i < GKbodyContent.length ; i++)
-{
-  GKcontainer.appendChild(GKbodyContent[i])
-}
+// const GKbodyContent = document.querySelectorAll("body > *")
+// const GKbody = document.querySelector("body")
+//
+// const GKcontainer = document.createElement("div")
+// const GKlightingLeft = document.createElement("div")
+// const GKlightingRight = document.createElement("div")
+// GKlightingLeft.classList.add("GKlighting", "left")
+// GKlightingRight.classList.add("GKlighting", "right")
+// GKbody.appendChild(GKlightingLeft)
+// GKbody.appendChild(GKlightingRight)
+//
+//
+//
+// GKbody.appendChild(GKcontainer)
+// GKbody.classList.add("GKbody")
+// GKcontainer.classList.add("GKcontainer")
+//
+// for (let i = 0 ; i < GKbodyContent.length ; i++)
+// {
+//   GKcontainer.appendChild(GKbodyContent[i])
+// }
 
 /*
 ** ROMAIN'S SANDBOX ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
