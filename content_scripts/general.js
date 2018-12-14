16994550f13c883b40f6746109e3709bfd721642
lucaslehot <lehotlucas@gmail.com>
2018-12-14 17:12:05 +0100

    auto scroll feature

--- content_scripts/general.js
+++ content_scripts/general.js
@@ -3,6 +3,73 @@
 */
 
 /*
+** AUTO SCROLLING
+*/
+
+let scrollY = 0;
+const GKbody = document.querySelector('body')
+const GKscrollZoneBottom = document.createElement('div')
+const GKscrollZoneTop = document.createElement('div')
+GKscrollZoneBottom.classList.add('GKscrollZoneBottom', 'GKscrollZone')
+GKscrollZoneTop.classList.add('GKscrollZoneTop', 'GKscrollZone')
+GKbody.appendChild(GKscrollZoneBottom)
+GKbody.appendChild(GKscrollZoneTop)
+
+const scrollUp = function(clientX)
+{
+  if (scrollY>0)
+  {
+    let diff = ((clientX/GKbody.clientWidth)*25)
+    scrollY-=diff
+    window.scrollTo(0,scrollY)
+  }
+}
+
+const scrollDown = function(clientX)
+{
+  if (scrollY<GKbody.clientHeight)
+  {
+    let diff = ((clientX/GKbody.clientWidth)*25)
+    scrollY+=diff
+    window.scrollTo(0,scrollY)
+  }
+}
+
+let scrollUpInterval
+let scrollDownInterval
+
+GKscrollZoneTop.addEventListener(
+  'mouseenter',
+  function(e)
+  {
+    let clientX = e.clientX
+    scrollUpInterval = setInterval(function(){scrollUp(clientX)}, 25)
+  }
+)
+GKscrollZoneTop.addEventListener(
+  'mouseleave',
+  function()
+  {
+    clearInterval(scrollUpInterval)
+  }
+)
+GKscrollZoneBottom.addEventListener(
+  'mouseenter',
+  function(e)
+  {
+    let clientX = e.clientX
+    scrollDownInterval = setInterval(function(){scrollDown(clientX)}, 25)
+  }
+)
+GKscrollZoneBottom.addEventListener(
+  'mouseleave',
+  function()
+  {
+    clearInterval(scrollDownInterval)
+  }
+)
+
+/*
 ** LUCAS' SANDBOX ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
@@ -16,6 +83,8 @@ for (let i = 0 ; i < GKclickableAll.length ; i++)
 {
   GKclickableAll[i].classList.add("GKclickable")
 }
+
+
 
 /*
 ** LIGHTING
--- content_scripts/twitter.js
+++ content_scripts/twitter.js
@@ -3,6 +3,73 @@
 */
 
 /*
+** AUTO SCROLLING
+*/
+
+let scrollY = 0;
+const GKbody = document.querySelector('body')
+const GKscrollZoneBottom = document.createElement('div')
+const GKscrollZoneTop = document.createElement('div')
+GKscrollZoneBottom.classList.add('GKscrollZoneBottom', 'GKscrollZone')
+GKscrollZoneTop.classList.add('GKscrollZoneTop', 'GKscrollZone')
+GKbody.appendChild(GKscrollZoneBottom)
+GKbody.appendChild(GKscrollZoneTop)
+
+const scrollUp = function(clientX)
+{
+  if (scrollY>0)
+  {
+    let diff = ((clientX/GKbody.clientWidth)*25)
+    scrollY-=diff
+    window.scrollTo(0,scrollY)
+  }
+}
+
+const scrollDown = function(clientX)
+{
+  if (scrollY<GKbody.clientHeight)
+  {
+    let diff = ((clientX/GKbody.clientWidth)*25)
+    scrollY+=diff
+    window.scrollTo(0,scrollY)
+  }
+}
+
+let scrollUpInterval
+let scrollDownInterval
+
+GKscrollZoneTop.addEventListener(
+  'mouseenter',
+  function(e)
+  {
+    let clientX = e.clientX
+    scrollUpInterval = setInterval(function(){scrollUp(clientX)}, 25)
+  }
+)
+GKscrollZoneTop.addEventListener(
+  'mouseleave',
+  function()
+  {
+    clearInterval(scrollUpInterval)
+  }
+)
+GKscrollZoneBottom.addEventListener(
+  'mouseenter',
+  function(e)
+  {
+    let clientX = e.clientX
+    scrollDownInterval = setInterval(function(){scrollDown(clientX)}, 25)
+  }
+)
+GKscrollZoneBottom.addEventListener(
+  'mouseleave',
+  function()
+  {
+    clearInterval(scrollDownInterval)
+  }
+)
+
+/*
 ** clickable things
 */
 
@@ -11,7 +78,6 @@ const GKclickableAll = document.querySelectorAll('button, a, h3')
 for (let i = 0 ; i < GKclickableAll.length ; i++)
 {
   GKclickableAll[i].classList.add("GKclickable")
-  GKclickableAll[i].classList.add("GKsacle")
 }
 
 /*
