ffabbf4ac12fcdaf02a86348cd2128ce51eab084
RomainKH <romain.khanoyan@gmail.com>
2018-12-16 16:22:46 +0100

    :art: multiplying buttons on options page & finishing basics utilities

--- options/options.js
+++ options/options.js
@@ -3,7 +3,9 @@ const content = container.querySelector(".content")
 const bigTitle = content.querySelector("h1")
 const generalPara = container.querySelector(".generalPara")
 const help = container.querySelector(".help")
-const subContent = content.querySelector(".subContent")
+const subContent = content.querySelector(".subContents")
+const contact = content.querySelector(".contact")
+const support = content.querySelector(".support")
 
 let timeOut = null
 const title = content.querySelector("h1")
@@ -34,12 +36,15 @@ selectorPara.addEventListener("mouseenter", () => {
         help.classList.add("animation_tabs_close")
         generalPara.classList.add("animation_open")
 
-        subContent.style.display = "flex"
+        support.style.display = "none"
+        contact.style.display = "none"
+        subContent.style.display = "block"
         title.innerHTML = "General Parameters"
         subtitle.innerHTML = "Change things you like so Ginkgo can help you feel better when navigating."
         subtitle.classList.add("opacity")
         title.classList.add("opacity")
         subContent.classList.add("opacity")
+        container.style.height = "830px"
 
         timeOut = setTimeout(function(){
             subtitle.classList.remove("opacity")
@@ -70,15 +75,22 @@ selectorHelp.addEventListener("mouseenter", () => {
         generalPara.classList.add("animation_tabs_close")
         help.classList.add("animation_open")
 
+        support.style.display = "flex"
+        contact.style.display = "block"
+        support.classList.add("opacity")
+        contact.classList.add("opacity")
         subContent.style.display = "none"
         title.innerHTML = "Help"
         title.classList.add("opacity")
         subtitle.innerHTML = "If you ever encounter a problem using our extension contact us and we'll take your feedback in consideration to make Ginkgo better for you !"
         subtitle.classList.add("opacity")
+        container.style.height = "750px"
         
         timeOut = setTimeout(function(){
             subtitle.classList.remove("opacity")
             title.classList.remove("opacity")
+            support.classList.remove("opacity")
+            contact.classList.remove("opacity")
         },299)
 
     }}, 1000)
@@ -89,58 +101,65 @@ selectorHelp.addEventListener("mouseenter", () => {
 
 
 /***
- * Button on hover switch
+ * Buttons on hover switch
  */
 
-const buttonSwitch = content.querySelector(".subContent button")
-const buttonSwitchAfter = content.querySelector(".subContent button .after")
-const buttonSwitchBefore = content.querySelector(".subContent button .before")
-let keyboard = localStorage.getItem("keyboard")
-if(keyboard == "off"){
-    buttonSwitchAfter.classList.add("moving")
-    buttonSwitchBefore.style.display = "none"
-}
-else{
-    buttonSwitchBefore.classList.add("moving")
-    buttonSwitchBefore.classList.add("animBefore")
-    buttonSwitchAfter.style.display = "none"
-}
-
-
-
-buttonSwitch.addEventListener("mouseenter", (_event) => {
-    if(buttonSwitchAfter.classList.contains("moving")){
-        buttonSwitchAfter.classList.add("animAfter")
+for (let i = 0; i < 3; i++) {
+    let buttonSwitch = content.querySelector(`.subContent${i} button`)
+    let buttonSwitchAfter = content.querySelector(`.subContent${i} button .after`)
+    let buttonSwitchBefore = content.querySelector(`.subContent${i} button .before`)
+    let parameters = localStorage.getItem(`parameter${i}`)
+    if(parameters == "0"){
+        buttonSwitchAfter.classList.add("moving")
+        buttonSwitchBefore.style.display = "none"
+    }
+    else if(parameters == "1"){
+        buttonSwitchBefore.classList.add("moving")
+        buttonSwitchBefore.classList.add("animBefore")
+        buttonSwitchAfter.style.display = "none"
     }
     else{
-        buttonSwitchAfter.classList.remove("animAfter")
+        localStorage.setItem(`parameter${i}`,`0`)
+        buttonSwitchAfter.classList.add("moving")
+        buttonSwitchBefore.style.display = "none"
     }
-    timeOut = setTimeout(function(){ 
+    buttonSwitch.addEventListener("mouseenter", () => {
         if(buttonSwitchAfter.classList.contains("moving")){
-            buttonSwitchAfter.classList.remove("moving")
-            buttonSwitchAfter.style.display = "none"
-            buttonSwitchBefore.style.display = "block"
-            console.log("keyboard on")
-            keyboard = localStorage.setItem("keyboard","on")
+            buttonSwitchAfter.classList.add("animAfter")
         }
         else{
-            buttonSwitchBefore.classList.remove("moving")
-            buttonSwitchBefore.style.display = "none"
-            buttonSwitchAfter.style.display = "block"
-            buttonSwitchAfter.classList.add("moving")
-            buttonSwitchBefore.classList.remove("animBefore")
-            console.log("keyboard off")
-            keyboard = localStorage.setItem("keyboard","off")
+            buttonSwitchAfter.classList.remove("animAfter")
         }
-     }, 999)
-    buttonSwitch.addEventListener("mouseleave", () => {
-        if(buttonSwitchAfter.classList.contains("moving")){
-
-        }
-        else{
-            buttonSwitchBefore.classList.add("moving")
-            buttonSwitchBefore.classList.add("animBefore")
-        }
-        window.clearTimeout(timeOut)
-    })
-})
+        timeOut = setTimeout(function(){ 
+            if(buttonSwitchAfter.classList.contains("moving")){
+                buttonSwitchAfter.classList.remove("moving")
+                buttonSwitchAfter.style.display = "none"
+                buttonSwitchBefore.style.display = "block"
+                console.log("keyboard on")
+                parameters = localStorage.setItem(`parameter${i}`,`1`)
+            }
+            else{
+                buttonSwitchBefore.classList.remove("moving")
+                buttonSwitchBefore.style.display = "none"
+                buttonSwitchAfter.style.display = "block"
+                buttonSwitchAfter.classList.add("moving")
+                buttonSwitchBefore.classList.remove("animBefore")
+                console.log("keyboard off")
+                parameters = localStorage.setItem(`parameter${i}`,`0`)
+            }
+         }, 799)
+        buttonSwitch.addEventListener("mouseleave", () => {
+            if(buttonSwitchAfter.classList.contains("moving")){
+    
+            }
+            else{
+                buttonSwitchBefore.classList.add("moving")
+                buttonSwitchBefore.classList.add("animBefore")
+            }
+            window.clearTimeout(timeOut)
+        })
+    })    
+}
+console.log(localStorage.getItem("parameter0"))
+console.log(localStorage.getItem("parameter1"))
+console.log(localStorage.getItem("parameter2"))
