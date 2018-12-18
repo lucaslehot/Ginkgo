0de8b34a6de3d14edd883081b47529925051f81f
RomainKH <romain.khanoyan@gmail.com>
2018-12-18 16:01:08 +0100

    :bug: fixing auto click on facebook

--- content_scripts/facebook.js
+++ content_scripts/facebook.js
@@ -8,7 +8,10 @@
 
 let scrollY = 0
 const GKbody = document.querySelector('body')
-const GKcontentArea = document.querySelector('#contentArea')
+let GKcontentArea = document.querySelector('#contentArea')
+if(GKcontentArea == null){
+  GKcontentArea = document.querySelector('#globalContainer')
+}
 const GKscrollZoneBottom = document.createElement('div')
 const GKscrollZoneTop = document.createElement('div')
 GKscrollZoneBottom.classList.add('GKscrollZoneBottom', 'GKscrollZone')
@@ -79,7 +82,7 @@ GKscrollZoneBottom.addEventListener(
 ** clickable things
 */
 
-const GKclickableAll = document.querySelectorAll('button, a:not(._4-eo), h3, ._ivp5, ._2ms2, ._nbt, .fbRemindersTitle, ._6vg, ._6a-y::before, ._2aha, ._mxb ')
+const GKclickableAll = document.querySelectorAll('button, a:not(._4-eo), ._ivp5, ._2ms2, ._nbt, .fbRemindersTitle, ._6vg, ._6a-y::before, ._2aha, ._mxb ')
 
 for (let i = 0 ; i < GKclickableAll.length ; i++)
 {
@@ -95,29 +98,14 @@ const config = {childList: true};
 
 const addClass = function(mutationsList)
 {
-  let toModify = document.querySelectorAll('button, a, h3')
+  let toModify = document.querySelectorAll('button, a, span')
 
   for (let i = 0 ; i < toModify.length ; i++)
   {
     if (!(toModify[i].classList.contains('GKclickable')))
     {
       toModify[i].classList.add("GKclickable")
-      console.log(toModify[i])
     }
-  }
-
-  for (let j = 0; j < GKclickableAll.length; j++) 
-  {
-    GKclickableAll[j].addEventListener('mouseenter', () => 
-    {
-      let timeOut
-      timeOut = setTimeout(function(){ 
-        GKclickableAll[j].click()
-      }, 1200)
-      GKclickableAll[j].addEventListener('mouseleave', () => {
-        window.clearTimeout(timeOut)
-      })
-    })
   }
 }
 
@@ -128,6 +116,33 @@ observer.observe (GKbody, config)
 /*
 ** ROMAIN'S SANDBOX ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
+
+
 chrome.storage.local.get('parameter0', function(result) {
   console.log(result.parameter0)
+})
+
+/*
+window.addEventListener('mousemove', (_event) =>{
+  const loop = () =>
+  {
+
+    window.requestAnimationFrame(loop)
+    underCursor = document.elementFromPoint(_event.clientX,_event.clientY)
+    if (underCursor.classList.contains("GKclickable")) {}
+    else {underCursor.classList.add("GKclickable")}
+  }
+})
+loop()
+let bgColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color')
+console.log(bgColor)
+*/
+window.addEventListener('mousemove', (_event) => {
+  let underCursor = document.elementFromPoint(_event.clientX,_event.clientY)
+  let timeOut
+  timeOut = setTimeout(function(){underCursor.click()}, 2000)
+  console.log(underCursor)
+  underCursor.addEventListener('mouseleave', ()=>{
+    window.clearTimeout(timeOut)
+  })
 })
