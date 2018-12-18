9a0e8ac283a824fc36da0af2d16b3800d4926718
lucaslehot <lehotlucas@gmail.com>
2018-12-18 14:12:18 +0100

    Keyboard structure

--- content_scripts/google.js
+++ content_scripts/google.js
@@ -95,6 +95,76 @@ for (let i = 0 ; i < GKclickableAll.length ; i++)
 */
 
 /*
+** VIRTUAL KEYBOARD
+*/
+
+const GKkeyboardContainer = document.createElement('div')
+GKkeyboardContainer.classList.add('GKkeyboardContainer')
+GKbody.appendChild(GKkeyboardContainer)
+const keyList = [
+                  ["1","key",""],              ["2","key",""],["3","key",""],["4","key",""],["5","key",""],["6","key",""],["7","key",""],["8","key",""],["9","key",""],["0","key",""],      ["Backspace","effect","twoColumns"],
+                  ["a","key",""],              ["z","key",""],["e","key",""],["r","key",""],["t","key",""],["y","key",""],["u","key",""],["i","key",""],["o","key",""],["p","key",""],      ["Enter","effect","twoColumns"],
+                  ["q","key",""],              ["s","key",""],["d","key",""],["f","key",""],["g","key",""],["h","key",""],["j","key",""],["k","key",""],["l","key",""],["m","key",""],      ["\'","key",""],                    ["\"","key",""],
+                  ["w","key",""],              ["x","key",""],["c","key",""],["v","key",""],["b","key",""],["n","key",""],[",","key",""],[".","key",""],["!","key",""],["?","key",""],      ["Up","effect",""],                 ["/","key",""],
+                  ["Maj","modifier","default"],[" ","key","fourColumns"],                                  ["(","key",""],[")","key",""],["-","key",""],["@","key",""],["Left","effect",""],["Down","effect",""],               ["Right","effect",""]
+               ]
+
+let clicking
+let majState = false
+keyList.forEach(function(element)
+{
+  let key = document.createElement('div')
+  key.innerHTML = element[0]
+  key.classList.add(element[1])
+  key.id = element[2]
+  key.addEventListener(
+    'mouseenter',
+    function()
+    {
+      key.classList.add('runAnimation')
+      const type = element[1]
+      clicking = window.setInterval(function(){keyEffect(key, type)}, 1000)
+    }
+  )
+  key.addEventListener(
+    'mouseleave',
+    function()
+    {
+      key.classList.remove('runAnimation')
+      void element.offsetWidth
+      window.clearInterval(clicking)
+    }
+  )
+  GKkeyboardContainer.appendChild(key)
+})
+
+const keyEffect = function (key, type)
+{
+  if (type == "key")
+  {
+
+  }
+  else if (type == "modifier")
+  {
+    if(key.id == "active")
+    {
+      key.id = "default"
+      majState = false
+    }
+    else
+    {
+      key.id = "active"
+      majState = true
+    }
+  }
+  else if (type == "effect")
+  {
+
+  }
+}
+
+
+/*
 ** ROMAIN'S SANDBOX ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
